aa0af15675b30159782a8a7c4d9e260176edb507
Codename <cburwell@gmail.com>
2017-06-02 20:18:11 -0400

    Added GCE storage upload

--- README.md
+++ README.md
@@ -4,24 +4,32 @@
 * See requirements.txt for more details
 
 ##Description
-This script will backup a PostgreSQL database. It currently only supports the pg_dumpall command for full backups.
+This script will backup a PostgreSQL database. It currently only supports the pg_dumpall command for full backups. There are built in provisions to encrypt the backup and upload the encrypted file to GCE or S3 storage.
 
 ##Usage
 ~~~~
-usage: postgresql-backup.py [-h] --dbhost DBHOST --dbun DBUN --dbpass DBPASS
-                            [--local LOCAL] [--remote REMOTE]
+usage: postgresql-backup.py [-h] [--action {backup,decrypt}] [--key KEY]
+                            [--dbhost DBHOST] [--dbun DBUN] [--dbpass DBPASS]
+                            [--local LOCAL] [--remote {gce,s3}]
+                            [--project PROJECT] [--bucket BUCKET]
 
 Backup a PostgreSQL database
 
-Note: The following arguments are required: --dbhost, --dbun, --dbpass
-
 optional arguments:
-  -h, --help       show this help message and exit
-  --dbhost DBHOST  Set the database hostname or IP address
-  --dbun DBUN      Set the username for connecting to the database
-  --dbpass DBPASS  Set the password for connecting to the database
-  --local LOCAL    Set the local location of the backup file. EX:
-                   /path/to/folder
-  --remote REMOTE  Set the remote location of the backup file. EX: gce or s3
-
+  -h, --help            show this help message and exit
+  --action {backup,decrypt}
+                        Action to take
+  --key KEY             Key used for encrypting the backup files
+  --dbhost DBHOST       Used for backups. Set the database hostname or IP
+                        address
+  --dbun DBUN           Used for backups. Set the username for connecting to
+                        the database
+  --dbpass DBPASS       Used for backups. Set the password for connecting to
+                        the database
+  --local LOCAL         Set the local location of the backup file. EX:
+                        /path/to/folder
+  --remote {gce,s3}     Used for backups. Set the remote location of the
+                        backup fileValid options are gce or s3
+  --project PROJECT     Used for backups. GCE project name
+  --bucket BUCKET       Used for backups. Cloud storage bucket name
 ~~~~
--- postgresql-backup.py
+++ postgresql-backup.py
@@ -5,6 +5,7 @@ import datetime
 import argparse
 import subprocess
 import encryption
+import gcestorage
 
 ################################################################################
 # Pseudocode
@@ -16,24 +17,26 @@ import encryption
 # XStore output in a variable
 # Check if there was and error when running the backup
 # XWrite output to a file
-# Compress & encrypt the file
-# Transfer file to GCE
+# XCompress & encrypt the file
+# XTransfer file to GCE
+# Handle GCE errors
 # Transfer file to S3
 ################################################################################
 # Main function
 def main():
-	# Set key/password for file encryption
-	encryption_key = "password"
-
 	# Check if the script is running as standalone
 	if __name__ == "__main__":
 		# Setup argument parser
 		parser = argparse.ArgumentParser(description='Backup a PostgreSQL database')
-		parser.add_argument('--dbhost', action='store', required=True, help='Set the database hostname or IP address')
-		parser.add_argument('--dbun', action='store', required=True, help='Set the username for connecting to the database')
-		parser.add_argument('--dbpass', action='store', required=True, help='Set the password for connecting to the database')
+		parser.add_argument('--action', action='store', required=False, choices=['backup', 'decrypt'], help='Action to take')
+		parser.add_argument('--key', action='store', required=False, help='Key used for encrypting the backup files')
+		parser.add_argument('--dbhost', action='store', required=False, help='Used for backups. Set the database hostname or IP address')
+		parser.add_argument('--dbun', action='store', required=False, help='Used for backups. Set the username for connecting to the database')
+		parser.add_argument('--dbpass', action='store', required=False, help='Used for backups. Set the password for connecting to the database')
 		parser.add_argument('--local', action='store', help='Set the local location of the backup file. EX: /path/to/folder')
-		parser.add_argument('--remote', action='store', help='Set the remote location of the backup file. EX: gce or s3')
+		parser.add_argument('--remote', action='store', choices=['gce', 's3'], help='Used for backups. Set the remote location of the backup fileValid options are gce or s3')
+		parser.add_argument('--project', action='store', help='Used for backups. GCE project name')
+		parser.add_argument('--bucket', action='store', help='Used for backups. Cloud storage bucket name')
 		args = parser.parse_args()
 
 		# Check if no arguments are passed
@@ -48,9 +51,11 @@ def main():
 
 		# Set the local backup file destination
 		if args.local == "":
+			# If the --local argument is empty, set the backup destination to the current directory
 			local_backup_dest = str(args.local)
 			print("[+] Setting local backup destination to: " + local_backup_dest)
 		else:
+			# Otherwise use the provided directory as the destination
 			local_backup_dest = str(os.getcwd())
 			print("[+] Setting local backup destination to: " + local_backup_dest)
 
@@ -58,17 +63,41 @@ def main():
 		backup_filename = str("{host}_full_backup_{date}_{time}.psql".format(host=db_host, date=time.strftime("%d%m%Y"), time=time.strftime("%H%M%S")))
 		backup_full_path = local_backup_dest + "/" + backup_filename
 		print("[+] Setting full path to: " + backup_full_path)
+
+		# Create the encrypted backup file name & full path
+		encrypted_backup_file_name = "ENCRYPTED_" + backup_filename
+		encrypted_backup_full_path = local_backup_dest + "/" + encrypted_backup_file_name
 		
 		# Run database backup
 		psql_full_dump(db_host, db_username, db_password, backup_full_path)
 
 		# Encrypt file
 		print("[+] Encrypting backup file")
-		encryption.encrypt_file(encryption_key, backup_filename)
+		encryption.encrypt_file(args.key, backup_filename)
+
+		# Delete Plain text file
+		print("[+] Deleting the plain text file " + backup_full_path)
+		os.remove(backup_full_path)
+
+		# Check if GCE was set as the remote backup destination
+		if args.remote == "gce":
+			# Upload encrypted file to GCE bucket
+			print("[+] Uploading encrypted file to GCE storage")
+			gcestorage.upload_to_bucket(args.project, args.bucket, encrypted_backup_full_path, encrypted_backup_file_name)
+		elif args.remote == "s3":
+			# Upload encrypted file to S3 bucket
+			print("[+] Uploading encrypted file to AWS S3 storage")
+		else:
+			# Invalid remote string provided
+			print("[-] Invalid --remote option! Skipping cloud upload")
+
+		# Download encrypted file from GCE bucket
+		#print("[+] Downloading encrypted file from GCE storage")
+		#gcestorage.download_from_bucket(gce_storage_project, gce_storage_bucket, backup_filename, backup_filename)
 
 		# Decrypt files
-		print("[+] Decrypting backup file")
-		encryption.decrypt_file(encryption_key, "ENCRYPTED_127.0.0.1_full_backup_24052017_181215.psql")
+		#print("[+] Decrypting backup file")
+		#encryption.decrypt_file(encryption_key, "ENCRYPTED_127.0.0.1_full_backup_24052017_181215.psql")
 
 # Function for rull PostgreSQL backup
 def psql_full_dump(dbhost, dbun, dbpass, destination):
@@ -76,7 +105,6 @@ def psql_full_dump(dbhost, dbun, dbpass, destination):
 	os.environ["PGPASSWORD"] = dbpass
 	
 	#Run the backup
-	#backup_process = subprocess.Popen(['pg_dumpall', '-h', dbhost, '-U', dbun], stdout=subprocess.PIPE)
 	backup_contents = subprocess.getoutput('pg_dumpall -h ' + dbhost + ' -U ' + dbun)
 
 	# Save output to the file
